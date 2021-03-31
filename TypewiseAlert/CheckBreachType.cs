8c1fde1b202fb64dd60506932d35c57a4d0fac67
RaghavendraSR06 <raghu99459@gmail.com>
2021-03-31 20:48:40 +0530

    Create CheckBreachType.cs

---
+++ TypewiseAlert/CheckBreachType.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+using static TypewiseAlert.BMSConstants;
+
+namespace TypewiseAlert
+{
+    public class CheckBreachLevel
+    {
+        public static BreachType CheckBreachType(double value, double lowerLimit, double upperLimit)
+        {
+            if (value < lowerLimit)
+            {
+                return BreachType.TooLow;
+            }
+            if (value > upperLimit)
+            {
+                return BreachType.TooHigh;
+            }
+            return BreachType.Normal;
+        }
+
+        public static BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
+        {
+            ICoolingType cooling = BMSInstanceCreator.GetInstance(coolingType.ToString()) as ICoolingType;
+            return CheckBreachType(temperatureInC, cooling.LowerLimit, cooling.UpperLimit);
+         
+        }      
+    }
+}
