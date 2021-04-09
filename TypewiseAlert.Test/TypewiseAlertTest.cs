a87b3c9f3da67ed581f62884836dbbc94dac456c
RaghavendraSR06 <raghu99459@gmail.com>
2021-04-09 09:05:12 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -40,6 +40,34 @@ namespace TypewiseAlert.Test
             Assert.True(CheckBreachLevel.CheckBreachType(40, 40, 40) ==
               BMSConstants.BreachType.Normal);
         }
-          
+
+        [Fact]
+        public void EmailAlertTest()
+        {
+            var test = new Battery(BMSConstants.CoolingType.PassiveCooling,"test");
+            BMSTypeAlert.CheckAndAlert(BMSConstants.AlertTarget.EmailAlert, test, 24);
+        }
+
+        [Fact]
+        public void ControllerAlertTest()
+        {
+            var test = new Battery(BMSConstants.CoolingType.PassiveCooling, "test");
+            BMSTypeAlert.CheckAndAlert(BMSConstants.AlertTarget.ControllerAlert, test, 24);
+        }
+
+        [Fact]
+        public void ConsoleAlertTest()
+        {
+            var test = new Battery(BMSConstants.CoolingType.PassiveCooling, "test");
+            BMSTypeAlert.CheckAndAlert(BMSConstants.AlertTarget.ConsoleAlert, test, 24);
+        }
+
+        [Fact]
+        public void FakeAlertTest()
+        {
+            var test = new Battery(BMSConstants.CoolingType.PassiveCooling, "test");
+            BMSTypeAlert.CheckAndAlert(BMSConstants.AlertTarget.ConsoleAlert, test, 24);
+            Assert.True(ConsoleAlert.IsNotified);
+        }
     }
 }
