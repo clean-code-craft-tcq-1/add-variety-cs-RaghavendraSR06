bc931c2330129842016285f6bf428ef5192f9c8a
RaghavendraSR06 <raghu99459@gmail.com>
2021-04-14 21:59:49 +0530

    Update ConsoleAlert.cs

--- TypewiseAlert/ConsoleAlert.cs
+++ TypewiseAlert/ConsoleAlert.cs
@@ -4,11 +4,11 @@ namespace TypewiseAlert
 {
    public class ConsoleAlert : IAlerter
     {
-        public static bool IsNotified = false;
+       
         public void Alert(BMSConstants.BreachType breachType)
         {
             Console.WriteLine($"Hi, the temperature is {breachType}\n");
-            IsNotified = true;
+            
         }
     }
 }
