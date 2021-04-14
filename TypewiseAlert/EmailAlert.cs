e25fcc612e5bb4f99438a56cddbf4c9b63b93069
RaghavendraSR06 <raghu99459@gmail.com>
2021-04-14 22:01:51 +0530

    Update EmailAlert.cs

--- TypewiseAlert/EmailAlert.cs
+++ TypewiseAlert/EmailAlert.cs
@@ -4,13 +4,11 @@ namespace TypewiseAlert
 {
     class EmailAlert : IAlerter
     {
-        public bool IsNotified = false;
+       
         public void Alert(BMSConstants.BreachType breachType)
         {
             const ushort header = 0xfeed;
             Console.WriteLine($"{header}{breachType}");
-            IsNotified = true;
         }
-
     }
 }
